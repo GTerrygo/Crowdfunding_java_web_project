06950a69023771de424e8b508c720f3a6791e1be
GTerry <gtw0604@outlook.com>
2020-11-24 05:21:55 -0500

    Commit Message

--- pro3-paypal/src/main/java/com/gtw/paypal/config/PaypalPaymentIntent.java
+++
@@ -1,9 +0,0 @@
-package com.gtw.paypal.config;
-
-/**
- * @author
- * @create 2020-11-22-2:31
- */
-public enum PaypalPaymentIntent {
-    sale, authorize, order
-}
--- pro3-paypal/src/main/java/com/gtw/paypal/config/PaypalPaymentMethod.java
+++
@@ -1,9 +0,0 @@
-package com.gtw.paypal.config;
-
-/**
- * @author
- * @create 2020-11-22-2:32
- */
-public enum PaypalPaymentMethod {
-    credit_card, paypal
-}
